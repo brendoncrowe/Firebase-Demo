d8cf35c128fc9acc57a844bfc3b705ac6990dc65
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-04-10 16:41:21 -0400

    started firebase authentication class

---
+++ Firebase-Demo/Firebase Services/AuthenticationSession.swift
@@ -0,0 +1,33 @@
+//
+//  AuthenticationSession.swift
+//  Firebase-Demo
+//
+//  Created by Brendon Crowe on 4/10/23.
+//  Copyright © 2023 Brendon Crowe. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthenticationSession {
+    
+    public func createNewUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authDataResult = authDataResult {
+                completion(.success(authDataResult))
+            }
+        }
+    }
+    
+    public func signInExistingUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authDataResult = authDataResult {
+                completion(.success(authDataResult))
+            }
+        }
+    }
+}
